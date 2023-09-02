c4e05aa8d889b13272fa8bbf89dc6b54560c38f8
Natnael772 <natnaeldeyas0@gmail.com>
2023-09-02 17:50:53 +0300

    Hero Component

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <>
+      <div className="hero-main-con">
+        <div className="hero-title">Your Personal AI assistant</div>
+        <div className="hero-desc">
+          From flawless marketing copy to in-depth research and everyday
+          business communications, HyperWrite helps you work smarter, faster,
+          and with ease.
+        </div>
+
+        <div className="hero-btn-con">
+          <a href="" className="hero-btn hero-btn-1">
+            Try for free
+          </a>
+          <a
+            href="
+            "
+            className="hero-btn hero-btn-2"
+          >
+            Go to Assistant <span className="hero-btn-span">New</span>
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Hero;
