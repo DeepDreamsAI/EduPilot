ba4bc826e6b5b4719ffd2e08aa930c10195853ff
Natnael772 <natnaeldeyas0@gmail.com>
2023-09-03 15:47:05 +0300

    Updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,16 @@ import "./App.css";
 import Nav from "./components/Nav/Nav";
 import Hero from "./components/Hero/Hero";
 import Chat from "./components/Chat/Chat";
+import Recommendation from "./components/Recommendation/Recommendation";
 // import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      {/* <Nav />
-      <Hero /> */}
-      <Chat />
+      <Nav />
+      {/* <Hero /> */}
+      {/* <Chat /> */}
+      <Recommendation />
     </>
   );
 }
